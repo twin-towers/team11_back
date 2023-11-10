560071111130122451101ea827dbb5b5707d2e92
alexey_pereverzev <aper-hari@list.ru>
2023-11-10 18:51:00 +0600

    friday_01 - boilerplate + docker + postgres settings

--- src/main/java/com/example/hackathon11/controller/UserController.java
+++ src/main/java/com/example/hackathon11/controller/UserController.java
@@ -2,15 +2,17 @@ package com.example.hackathon11.controller;
 
 import com.example.hackathon11.entity.User;
 import com.example.hackathon11.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class UserController {
-
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
 
     @GetMapping("count-users")
