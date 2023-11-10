232f4089eddc5211f1336c46b139065a7d946735
alexey_pereverzev <aper-hari@list.ru>
2023-11-10 15:50:28 +0600

    initial

---
+++ src/main/java/com/example/hackathon11/Hackathon11Application.java
@@ -0,0 +1,13 @@
+package com.example.hackathon11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackathon11/Hackathon11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
